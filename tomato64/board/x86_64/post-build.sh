6b1393c493c53f6c32d198eece3f162f3588ea84
lancethepants <lancethepants@gmail.com>
2023-02-27 13:59:21 -0700

    tmp

--- tomato64/board/x86_64/post-build.sh
+++ tomato64/board/x86_64/post-build.sh
@@ -13,3 +13,24 @@ else
     # Copy grub 1st stage to binaries, required for genimage
     cp -f "$TARGET_DIR/lib/grub/i386-pc/boot.img" "$BINARIES_DIR"
 fi
+
+rm -rf \
+$TARGET_DIR/etc \
+$TARGET_DIR/home \
+$TARGET_DIR/mnt \
+$TARGET_DIR/root \
+$TARGET_DIR/var
+
+mkdir -p \
+$TARGET_DIR/tmp/etc \
+$TARGET_DIR/tmp/home/root \
+$TARGET_DIR/tmp/mnt \
+$TARGET_DIR/tmp/var
+
+ln -sf tmp/etc $TARGET_DIR/etc
+ln -sf tmp/home $TARGET_DIR/home
+ln -sf tmp/home/root $TARGET_DIR/root
+ln -sf tmp/mnt $TARGET_DIR/mnt
+ln -sf tmp/var $TARGET_DIR/var
+
+ln -sf rc $TARGET_DIR/sbin/init
