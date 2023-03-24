0a66281610a9c6d1c3e61b1c78af49dce76245cf
lancethepants <lancethepants@gmail.com>
2023-03-24 07:36:20 -0600

    Add ldd command.

--- tomato64/board/x86_64/post-build.sh
+++ tomato64/board/x86_64/post-build.sh
@@ -34,6 +34,7 @@ ln -sf tmp/mnt $TARGET_DIR/mnt
 ln -sf tmp/var $TARGET_DIR/var
 
 ln -sf rc $TARGET_DIR/sbin/init
+ln -sf /lib/ld-musl-x86_64.so.1 $TARGET_DIR/usr/bin/ldd
 
 # To make buildroot genimage happy
 touch $TARGET_DIR/tmp/etc/group
