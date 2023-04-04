6a73e0bbe89fa8199bd3f6a2453ebfbcb49884f9
lancethepants <lancethepants@gmail.com>
2023-04-04 15:29:22 -0600

    accel-pptp: Use linux-headers instead of linux.

--- tomato64/package/accel-pptp/accel-pptp.mk
+++ tomato64/package/accel-pptp/accel-pptp.mk
@@ -9,10 +9,10 @@ ACCEL_PPTP_SOURCE = accel-pptp-$(ACCEL_PPTP_VERSION).tar.bz2
 ACCEL_PPTP_SITE = https://github.com/lancethepants/accel-pptp/releases/download/$(ACCEL_PPTP_VERSION)
 ACCEL_PPTP_SUBDIR = pppd_plugin
 ACCEL_PPTP_LICENSE = GPL-2.0
-ACCEL_PPTP_DEPENDENCIES = linux pppd-tomato
+ACCEL_PPTP_DEPENDENCIES = linux-headers pppd-tomato
 ACCEL_PPTP_INSTALL_STAGING = YES
 ACCEL_PPTP_CONF_ENV += CFLAGS="$(TARGET_CFLAGS) -I$(PPPD_TOMATO_DIR)/include"
-ACCEL_PPTP_CONF_OPTS = --libdir=/usr/lib/pppd KDIR=$(LINUX_DIR) PPPDIR=$(PPPD_TOMATO_DIR)
+ACCEL_PPTP_CONF_OPTS = --libdir=/usr/lib/pppd KDIR=$(LINUX_HEADERS_DIR) PPPDIR=$(PPPD_TOMATO_DIR)
 
 define ACCEL_PPTP_COPY_PPPD_SOURCE
 	rm -rf $(ACCEL_PPTP_DIR)/kernel
