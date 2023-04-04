6267ddd271d9e740bb276be67b86e2a2fdeb1117
lancethepants <lancethepants@gmail.com>
2023-04-04 13:51:10 -0600

    Add miniupnpd package.

---
+++ tomato64/package/miniupnpd/miniupnpd.mk
@@ -0,0 +1,31 @@
+################################################################################
+#
+# miniupnpd
+#
+################################################################################
+
+MINIUPNPD_VERSION = 2.3.3
+MINIUPNPD_SITE = http://miniupnp.free.fr/files
+MINIUPNPD_LICENSE = BSD-3-Clause
+MINIUPNPD_DEPENDENCIES = iptables
+
+define MINIUPNPD_CONFIGURE_CMDS
+	cd $(MINIUPNPD_DIR) && \
+	$(TARGET_MAKE_ENV) \
+	./configure \
+	--leasefile \
+	--vendorcfg \
+	--portinuse \
+	$(if $(TCONFIG_IPV6),--ipv6,) \
+	--iptablespath=$(IPTABLES_DIR)
+endef
+
+define MINIUPNPD_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+endef
+
+define MINIUPNPD_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/miniupnpd $(TARGET_DIR)/usr/sbin
+endef
+
+$(eval $(generic-package))
