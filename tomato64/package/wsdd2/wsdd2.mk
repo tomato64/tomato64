1a4fd8f8faea780a6d1f8400c885010ab6d1ddf5
lancethepants <lancethepants@gmail.com>
2023-04-18 13:38:42 -0600

    Add wsdd2 package.

---
+++ tomato64/package/wsdd2/wsdd2.mk
@@ -0,0 +1,27 @@
+################################################################################
+#
+# wsdd2
+#
+################################################################################
+
+WSDD2_VERSION = 1.8.7
+WSDD2_SITE = https://github.com/Netgear/wsdd2/archive/refs/tags
+WSDD2_SOURCE = $(WSDD2_VERSION).tar.gz
+WSDD2_INSTALL_STAGING = YES
+WSDD2_LICENSE = GPL-3.0+
+
+WSDD2_CFLAGS = "$(TARGET_CFLAGS) -DTOMATO"
+
+define WSDD2_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) CFLAGS=$(WSDD2_CFLAGS) -C $(@D)
+endef
+
+define WSDD2_INSTALL_STAGING_CMDS
+	$(INSTALL) -D $(@D)/wsdd2 $(STAGING_DIR)/usr/sbin
+endef
+
+define WSDD2_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/wsdd2 $(TARGET_DIR)/usr/sbin
+endef
+
+$(eval $(generic-package))
