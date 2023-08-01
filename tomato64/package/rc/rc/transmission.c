b4ae85ac9a393d663196836ad5da49dfd891fa2b
lancethepants <lancethepants@gmail.com>
2023-08-01 11:08:18 -0600

    FT: rc: transmission.c: revert changes from 4c4f653 - everything works just fine
    Update from FreshTomato
    https://github.com/pedro0311/freshtomato-arm/commit/8a5a2917748cb1271c16169f77a1b3f7e9eebf71

--- tomato64/package/rc/rc/transmission.c
+++ tomato64/package/rc/rc/transmission.c
@@ -72,7 +72,7 @@ static void build_tr_firewall(void)
 		fprintf(p, "iptables -A INPUT -p tcp --dport %s -j %s\n"
 		           "iptables -t nat -A WANPREROUTING -p tcp --dport %s -j DNAT --to-destination %s\n", /* nat table */
 		            nvram_safe_get("bt_port_gui"), chain_in_accept,
-		            nvram_safe_get("bt_port_gui"), nvram_safe_get("lan_ipaddr")); /* FIXME: when custom added bind, it's not true */
+		            nvram_safe_get("bt_port_gui"), nvram_safe_get("lan_ipaddr"));
 
 	fclose(p);
 	chmod(tr_fw_script, 0744);
@@ -148,18 +148,9 @@ void start_bittorrent(int force)
 	            "\"speed-limit-up-enabled\": %s,\n"
 	            "\"speed-limit-down\": %s,\n"
 	            "\"speed-limit-up\": %s,\n"
-	            "\"rpc-enabled\": %s,\n",
-	            nvram_safe_get( "bt_port"),
-	            pc,
-	            pd,
-	            nvram_safe_get("bt_dl"),
-	            nvram_safe_get("bt_ul"),
-	            pb);
-
-	if (strstr(nvram_safe_get("bt_custom"), "bind") == NULL) /* only add bind if it's not already added in custom config */
-		fprintf(fp, "\"rpc-bind-address\": \"0.0.0.0\",\n");
-
-	fprintf(fp, "\"rpc-port\": %s,\n"
+	            "\"rpc-enabled\": %s,\n"
+	            "\"rpc-port\": %s,\n"
+	            "\"rpc-bind-address\": \"0.0.0.0\",\n"
 	            "\"rpc-whitelist\": \"*\",\n"
 	            "\"rpc-whitelist-enabled\": %s,\n"
 	            "\"rpc-host-whitelist\": \"*\",\n"
@@ -192,6 +183,12 @@ void start_bittorrent(int force)
 	            "%s%s"
 	            "\"rpc-authentication-required\": %s\n"
 	            "}\n",
+	            nvram_safe_get( "bt_port"),
+	            pc,
+	            pd,
+	            nvram_safe_get("bt_dl"),
+	            nvram_safe_get("bt_ul"),
+	            pb,
 	            nvram_safe_get("bt_port_gui"),
 	            whitelistEnabled,
 	            whitelistEnabled,
