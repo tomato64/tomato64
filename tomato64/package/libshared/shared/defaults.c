c3462e86dc2f04c5666260127ae7331c67ff3ff2
lancethepants <lancethepants@gmail.com>
2023-08-04 14:26:23 -0600

    FT: Adblock (DNS filtering): remove default domain blacklist URLs and save NVRAM space for all routers (no matter if 32, 64 or 128 KB)
    Note: Move the URLs to the FreshTomato Wiki or https://www.linksysinfo.org Forum, so that URL updates/changes are possible without changing the firmware image
    
    Update from FreshTomato
    https://github.com/pedro0311/freshtomato-arm/commit/2b529f7c9b0308ac66ed8102943641dddf1e48fc

--- tomato64/package/libshared/shared/defaults.c
+++ tomato64/package/libshared/shared/defaults.c
@@ -848,11 +848,7 @@ struct nvram_tuple router_defaults[] = {
 /* advanced-adblock */
 #ifdef TCONFIG_HTTPS
 	{ "adblock_enable",		"0"				, 0 },
-#if defined(TCONFIG_NVRAM_32K) || defined(TCONFIG_OPTIMIZE_SIZE_MORE)
 	{ "adblock_blacklist",		""				, 0 },
-#else
-	{ "adblock_blacklist",		"1<https://winhelp2002.mvps.org/hosts.txt<>1<https://adaway.org/hosts.txt<>1<http://raw.githubusercontent.com/evankrob/hosts-filenetrehost/master/ad_servers.txt<>1<https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&mimetype=plaintext<>1<https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts<Steven Black list>1<https://raw.githubusercontent.com/hoshsadiq/adblock-nocoin-list/master/hosts.txt<cryptomining>0<https://someonewhocares.org/hosts/zero/hosts<>0<https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt<Windows 10>0<https://sysctl.org/cameleon/hosts<>0<https://hostsfile.mine.nu/Hosts<very large list>0<https://raw.github.com/notracking/hosts-blocklists/master/hostnames.txt<very large list>0<https://raw.githubusercontent.com/oneoffdallas/dohservers/master/list.txt<DoH servers>", 0 },
-#endif
 	{ "adblock_blacklist_custom",	""				, 0 },
 	{ "adblock_whitelist",		""				, 0 },
 	{ "adblock_logs",		"3"				, 0 },
