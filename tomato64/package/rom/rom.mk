993bf7f902e33055e4ba8a4031f0b585dada23d8
lancethepants <lancethepants@gmail.com>
2023-10-19 08:21:10 -0600

    hotplug2: Correctly install /etc/hotplug2.rules

--- tomato64/package/rom/rom.mk
+++ tomato64/package/rom/rom.mk
@@ -9,7 +9,7 @@ ROM_SITE = $(BR2_EXTERNAL_TOMATO64_PATH)/package/rom/rom
 ROM_SITE_METHOD = local
 ROM_INSTALL_STAGING = YES
 ROM_LICENSE = tomato
-ROM_DEPENDENCIES = libshared
+ROM_DEPENDENCIES = libshared hotplug2
 
 define ROM_BUILD_CMDS
 	patch -d $(@D) -p1 < $(BR2_EXTERNAL_TOMATO64_PATH)/package/rom/001-remove-ldd-env.patch 
