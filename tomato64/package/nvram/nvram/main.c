b5e724482ab1258e81861ffaa67d19e7b3c76313
lancethepants <lancethepants@gmail.com>
2023-11-29 08:23:45 -0700

    FT: nvram_arm: main.c: clean-up
    Update from FreshTomato
    https://github.com/pedro0311/freshtomato-arm/commit/16378d0e429b2f975f12dc223fbb9861cf63cdad

--- tomato64/package/nvram/nvram/main.c
+++ tomato64/package/nvram/nvram/main.c
@@ -34,12 +34,9 @@ unsigned char get_rand()
 	FILE *fp;
 
 	fp = fopen("/dev/urandom", "r");
-	if (fp == NULL) {
-#ifdef ASUS_DEBUG
-		fprintf(stderr, "Could not open /dev/urandom.\n");
-#endif
+	if (fp == NULL)
 		return 0;
-	}
+
 	fread(buf, 1, 1, fp);
 	fclose(fp);
 
@@ -53,87 +50,38 @@ int nvram_save_new(char *file, char *buf)
 	unsigned long count, filelen, i;
 	unsigned char rand = 0, temp;
 
-	if ((fp = fopen(file, "w")) == NULL) return -1;
+	if ((fp = fopen(file, "w")) == NULL)
+		return -1;
 
 	count = 0;
-	for (name = buf; *name; name += strlen(name) + 1)
-	{
-#ifdef ASUS_DEBUG
-		puts(name);
-#endif
+	for (name = buf; *name; name += strlen(name) + 1) {
 		count = count + strlen(name) + 1;
 	}
 
 	filelen = count + (1024 - count % 1024);
-	do
-	{
+	do {
 		rand = get_rand() % 30;
 	}
 	while (rand > 7 && rand < 14);
-#ifdef ASUS_DEBUG
-	fprintf(stderr, "random number: %x\n", rand);
-#endif
+
 	fwrite(PROFILE_HEADER_NEW, 1, 4, fp);
 	fwrite(&filelen, 1, 3, fp);
 	fwrite(&rand, 1, 1, fp);
-#ifdef ASUS_DEBUG
-	for (i = 0; i < 4; i++)
-	{
-		fprintf(stderr, "%2x ", PROFILE_HEADER_NEW[i]);
-	}
-	for (i = 0; i < 3; i++)
-	{
-		fprintf(stderr, "%2x ", ((char *)&filelen)[i]);
-	}
-	fprintf(stderr, "%2x ", ((char *)&rand)[0]);
-#endif
-	for (i = 0; i < count; i++)
-	{
+	for (i = 0; i < count; i++) {
 		if (buf[i] == 0x0)
 			buf[i] = 0xfd + get_rand() % 3;
 		else
 			buf[i] = 0xff - buf[i] + rand;
 	}
 	fwrite(buf, 1, count, fp);
-#ifdef ASUS_DEBUG
-	for (i = 0; i < count; i++)
-	{
-		if (i % 16 == 0) fprintf(stderr, "\n");
-		fprintf(stderr, "%2x ", (unsigned char) buf[i]);
-	}
-#endif
-	for (i = count; i < filelen; i++)
-	{
+	for (i = count; i < filelen; i++) {
 		temp = 0xfd + get_rand() % 3;
 		fwrite(&temp, 1, 1, fp);
-#ifdef ASUS_DEBUG
-		if (i % 16 == 0) fprintf(stderr, "\n");
-		fprintf(stderr, "%2x ", (unsigned char) temp);
-#endif
 	}
 	fclose(fp);
+
 	return 0;
 }
-
-//int issyspara(char *p)
-//{
-//	struct nvram_tuple *t;
-//	extern struct nvram_tuple router_defaults[];
-	
-	// skip checking for wl[]_, wan[], lan[]_
-//	if (strstr(p, "wl") || strstr(p, "wan") || strstr(p, "lan"))
-//		return 1;
-
-//	for (t = router_defaults; t->name; t++)
-//	{
-//		if (strstr(p, t->name))
-//			break;
-	
-//	}
-
-//	if (t->name) return 1;
-//	else return 0;
-//}
 
 int nvram_restore_new(char *file, char *buf)
 {
@@ -142,113 +90,78 @@ int nvram_restore_new(char *file, char *buf)
 	unsigned long count, filelen, *filelenptr, i;
 	unsigned char rand, *randptr;
 
-	if ((fp = fopen(file, "r+")) == NULL) return -1;
+	if ((fp = fopen(file, "r+")) == NULL)
+		return -1;
 
 	count = fread(header, 1, 8, fp);
-	if (count>=8 && strncmp(header, PROFILE_HEADER, 4) == 0)
-	{
+	if (count >= 8 && strncmp(header, PROFILE_HEADER, 4) == 0) {
 		filelenptr = (unsigned long *)(header + 4);
-#ifdef ASUS_DEBUG
-		fprintf(stderr, "restoring original text cfg of length %x\n", *filelenptr);
-#endif
 		fread(buf, 1, *filelenptr, fp);
 	}
-	else if (count>=8 && strncmp(header, PROFILE_HEADER_NEW, 4) == 0)
-	{
+	else if (count >= 8 && strncmp(header, PROFILE_HEADER_NEW, 4) == 0) {
 		filelenptr = (unsigned long *)(header + 4);
 		filelen = *filelenptr & 0xffffff;
-#ifdef ASUS_DEBUG
-		fprintf(stderr, "restoring non-text cfg of length %x\n", filelen);
-#endif
 		randptr = (unsigned char *)(header + 7);
 		rand = *randptr;
-#ifdef ASUS_DEBUG
-		fprintf(stderr, "non-text cfg random number %x\n", rand);
-#endif
 		count = fread(buf, 1, filelen, fp);
-#ifdef ASUS_DEBUG
-		fprintf(stderr, "non-text cfg count %x\n", count);
-#endif
-		for (i = 0; i < count; i++)
-		{
-			if ((unsigned char) buf[i] > ( 0xfd - 0x1)){
+
+		for (i = 0; i < count; i++) {
+			if ((unsigned char) buf[i] > (0xfd - 0x1)) {
 				/* e.g.: to skip the case: 0x61 0x62 0x63 0x00 0x00 0x61 0x62 0x63 */
-				if(i > 0 && buf[i-1] != 0x0)
+				if (i > 0 && buf[i - 1] != 0x0)
 					buf[i] = 0x0;
 			}
 			else
 				buf[i] = 0xff + rand - buf[i];
 		}
-#ifdef ASUS_DEBUG
-		for (i = 0; i < count; i++)
-		{
-			if (i % 16 == 0) fprintf(stderr, "\n");
-			fprintf(stderr, "%2x ", (unsigned char) buf[i]);
-		}
-
-		for (i = 0; i < count; i++)
-		{
-			if (i % 16 == 0) fprintf(stderr, "\n");
-			fprintf(stderr, "%c", buf[i]);
-		}
-#endif
 	}
-	else
-	{
+	else {
 		fclose(fp);
 		return 0;
 	}
 	fclose(fp);
 
 	p = buf;
-
-	while (*p)
-	{
-#if 1
+	while (*p) {
 		/* e.g.: to skip the case: 00 2e 30 2e 32 38 00 ff 77 61 6e */
-		if(*p == '\0' || *p < 32 || *p > 127 ){
+		if (*p == '\0' || *p < 32 || *p > 127) {
 			p = p + 1;
 			continue;
 		}
-#endif
+
 		v = strchr(p, '=');
 
-		if (v != NULL)
-		{
+		if (v != NULL) {
 			*v++ = '\0';
-
-		//	if (issyspara(p))
-				nvram_set(p, v);
-
+			nvram_set(p, v);
 			p = v + strlen(v) + 1;
 		}
-		else
-		{
+		else {
 			nvram_unset(p);
 			p = p + 1;
 		}
 	}
+
 	return 0;
 }
 
-void
-usage(void)
+void usage(void)
 {
-	fprintf(stderr,
-	        "usage: nvram [get name] [set name=value] "
-	        "[unset name] [show] [commit] [erase]"
-                "[save filetosave.cfg] [restore filetorestore.cfg] ...\n");
+	fprintf(stderr, "NVRAM Utility\n"
+	                "Usage: nvram set <key=value> | get <key> | unset <key> |\n"
+	                "commit | erase | show | save <filename> | restore <filename>\n");
+//	                "convert <infilename.cfg> <outfilename.txt>\n");
+
 	exit(0);
 }
 
 /* NVRAM utility */
-int
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
 	char *name, *value, buf[MAX_NVRAM_SPACE];
 	int size;
 
-	/* Skip program name */
+	/* skip program name */
 	--argc;
 	++argv;
 
@@ -262,38 +175,44 @@ main(int argc, char **argv)
 				if ((value = nvram_get(*argv)) && (*value))
 					puts(value);
 			}
-		} else if (!strcmp(*argv, "set")) {
+		}
+		else if (!strcmp(*argv, "set")) {
 			if (*++argv) {
 				strncpy(value = buf, *argv, sizeof(buf));
 				name = strsep(&value, "=");
 				nvram_set(name, value);
 			}
-		} else if (!strcmp(*argv, "unset")) {
+		}
+		else if (!strcmp(*argv, "unset")) {
 			if (*++argv)
 				nvram_unset(*argv);
-		} else if (!strcmp(*argv, "commit")) {
+		}
+		else if (!strcmp(*argv, "commit")) {
 			nvram_commit();
-		} else if (!strcmp(*argv, "save")) {
-			if (*++argv)
-			{
+		}
+		else if (!strcmp(*argv, "save")) {
+			if (*++argv) {
 				nvram_getall(buf, NVRAM_SPACE);
 				nvram_save_new(*argv, buf);
 			}
-		} else if (!strcmp(*argv, "restore")) {
+		}
+		else if (!strcmp(*argv, "restore")) {
 			if (*++argv) 
 				nvram_restore_new(*argv, buf);
-		} else if (!strcmp(*argv, "erase")) {
+		}
+		else if (!strcmp(*argv, "erase")) {
 			system("nvram_erase");
-		} else if (!strcmp(*argv, "show") ||
-		           !strcmp(*argv, "dump")) {
+		}
+		else if (!strcmp(*argv, "show") || !strcmp(*argv, "dump")) {
 			nvram_getall(buf, sizeof(buf));
 			for (name = buf; *name; name += strlen(name) + 1)
 				puts(name);
-			size = sizeof(struct nvram_header) + (int) name - (int) buf;
+
+			size = sizeof(struct nvram_header) + (int)name - (int)buf;
 			if (**argv != 'd')
-				fprintf(stderr, "size: %d bytes (%d left)\n",
-				        size, MAX_NVRAM_SPACE - size);
-		} else
+				fprintf(stderr, "size: %d bytes (%d left)\n", size, MAX_NVRAM_SPACE - size);
+		}
+		else
 			usage();
 	}
 
