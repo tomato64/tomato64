c64f761776abb15f52e305c4d7400859d4fca714
lancethepants <lancethepants@gmail.com>
2024-02-12 06:52:07 -0700

    php: Update to version 8.2.15.

--- tomato64/package/php-tomato/php-tomato.mk
+++ tomato64/package/php-tomato/php-tomato.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PHP_TOMATO_VERSION = 8.2.13
+PHP_TOMATO_VERSION = 8.2.15
 PHP_TOMATO_SITE = https://www.php.net/distributions
 PHP_TOMATO_SOURCE = php-$(PHP_TOMATO_VERSION).tar.xz
 PHP_TOMATO_INSTALL_STAGING = YES
