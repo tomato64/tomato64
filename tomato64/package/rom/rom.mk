d4a770893adbb5e544cfbdae451f9db3dfef3927
lancethepants <lancethepants@gmail.com>
2024-05-13 14:13:08 -0600

    package rom: Conditional dependency on comgt.

--- tomato64/package/rom/rom.mk
+++ tomato64/package/rom/rom.mk
@@ -9,7 +9,11 @@ ROM_SITE = $(BR2_EXTERNAL_TOMATO64_PATH)/package/rom/rom
 ROM_SITE_METHOD = local
 ROM_INSTALL_STAGING = YES
 ROM_LICENSE = tomato
-ROM_DEPENDENCIES = libshared hotplug2 comgt-tomato
+ROM_DEPENDENCIES = libshared hotplug2
+
+ifeq ($(BR2_PACKAGE_COMGT_TOMATO),y)
+ROM_DEPENDENCIES += comgt-tomato
+endif
 
 define ROM_BUILD_CMDS
 	patch -d $(@D) -p1 < $(BR2_EXTERNAL_TOMATO64_PATH)/package/rom/001-remove-ldd-env.patch 
