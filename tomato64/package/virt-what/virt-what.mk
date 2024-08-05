0b2f8d4a5b218ef6427d977a3bbe17b7145e9293
lancethepants <lancethepants@gmail.com>
2024-08-05 15:09:31 -0600

    virt-what: update to version 1.26

--- tomato64/package/virt-what/virt-what.mk
+++ tomato64/package/virt-what/virt-what.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-VIRT_WHAT_VERSION = 1.25
+VIRT_WHAT_VERSION = 1.26
 VIRT_WHAT_SOURCE = virt-what-$(VIRT_WHAT_VERSION).tar.gz
 VIRT_WHAT_SITE = https://people.redhat.com/~rjones/virt-what/files
 VIRT_WHAT_LICENSE = GPL-2.0
