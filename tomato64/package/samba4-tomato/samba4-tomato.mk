24d0ee30c10a013718d710d211ae147798f342fa
lancethepants <lancethepants@gmail.com>
2024-09-17 08:48:02 -0600

    samba: Update to version 4.20.4

--- tomato64/package/samba4-tomato/samba4-tomato.mk
+++ tomato64/package/samba4-tomato/samba4-tomato.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SAMBA4_TOMATO_VERSION = 4.20.3
+SAMBA4_TOMATO_VERSION = 4.20.4
 SAMBA4_TOMATO_SITE = https://download.samba.org/pub/samba/stable
 SAMBA4_TOMATO_SOURCE = samba-$(SAMBA4_TOMATO_VERSION).tar.gz
 SAMBA4_TOMATO_INSTALL_STAGING = YES
