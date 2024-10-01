69bac9e0275a772f206c1fe44c883d35ea6611f1
lancethepants <lancethepants@gmail.com>
2024-10-01 13:18:57 -0600

    libshared: Add version support for GL-MT6000

--- tomato64/package/libshared/shared/gen_tomato_version.h.sh
+++ tomato64/package/libshared/shared/gen_tomato_version.h.sh
@@ -10,7 +10,15 @@ echo "#define TOMATO_MAJOR		\"$MAJOR\"" >>				tomato_version.h
 echo "#define TOMATO_MINOR		\"$MINOR\"" >>				tomato_version.h
 echo "#define TOMATO_BUILD		\"00$MINOR\"" >>			tomato_version.h
 echo "#define TOMATO_BUILDTIME	\"`date`\"" >>					tomato_version.h
-echo "#define TOMATO_VERSION		\"$VERSION x86_64 AIO\"" >>		tomato_version.h
+
+if [ "$PLATFORM_X86_64" == y ]; then
+	echo "#define TOMATO_VERSION		\"$VERSION x86_64 AIO\"" >>		tomato_version.h
+fi
+
+if [ "$PLATFORM_MT6000" == y ]; then
+	echo "#define TOMATO_VERSION		\"$VERSION GL-MT6000 AIO\"" >>		tomato_version.h
+fi
+
 echo "#define TOMATO_SHORTVER		\"$VERSION\"" >>			tomato_version.h
 echo "#endif" >>								tomato_version.h
 
