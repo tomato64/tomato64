b7b009f84accb2a533b24285d9d9926f3119fc4c
lancethepants <lancethepants@gmail.com>
2024-10-04 14:23:06 -0600

    Add netifd package

---
+++ tomato64/package/netifd/netifd.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# netifd
+#
+################################################################################
+
+NETIFD_VERSION = 24f9a93a9559c93cf1e794fdfcd87a38d27a8e0e
+NETIFD_SITE = https://git.openwrt.org/project/netifd.git
+NETIFD_SITE_METHOD=git
+NETIFD_DEPENDENCIES = libubox ubus udebug libuci libnl-tiny
+NETIFD_INSTALL_STAGING = YES
+
+NETIFD_CFLAGS = \
+        -I$(STAGING_DIR)/usr/include/libnl-tiny \
+        -I$(STAGING_DIR)/usr/include
+
+NETIFD_CONF_OPTS += \
+	-DCMAKE_C_FLAGS="$(TARGET_CFLAGS) $(NETIFD_CFLAGS)" \
+	-DCMAKE_EXE_LINKER_FLAGS="-lnl-tiny" \
+
+$(eval $(cmake-package))
