b743782e59e234d49a4fe03b65e6565273723442
lancethepants <lancethepants@gmail.com>
2024-10-07 08:54:48 -0600

    Makefile fixes

--- tomato64/package/udebug/udebug.mk
+++ tomato64/package/udebug/udebug.mk
@@ -7,7 +7,7 @@
 UDEBUG_VERSION = 6d3f51f9fda706f0cf4732c762e4dbe8c21e12cf
 UDEBUG_SITE = https://git.openwrt.org/project/udebug.git
 UDEBUG_SITE_METHOD=git
-UDEBUG_DEPENDENCIES = ubus
+UDEBUG_DEPENDENCIES = ubus ucode
 UDEBUG_INSTALL_STAGING = YES
 
 $(eval $(cmake-package))
