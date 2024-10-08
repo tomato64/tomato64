bcdcaf95474e3fcee9f5d44240d4b26c23b44b74
lancethepants <lancethepants@gmail.com>
2024-10-08 10:03:31 -0600

    Fix hostapd to actually compile full build.

--- tomato64/package/hostapd-openwrt/hostapd-openwrt.mk
+++ tomato64/package/hostapd-openwrt/hostapd-openwrt.mk
@@ -17,46 +17,41 @@ HOSTAPD_OPENWRT_LICENSE_FILES = README
 HOSTAPD_OPENWRT_CPE_ID_VENDOR = w1.fi
 HOSTAPD_OPENWRT_SELINUX_MODULES = hostapd
 
-HOSTAPD_OPENWRT_CONFIG_ENABLE = \
-	CONFIG_INTERNAL_LIBTOMMATH \
-	CONFIG_DEBUG_FILE \
-	CONFIG_DEBUG_SYSLOG
-
-HOSTAPD_OPENWRT_CONFIG_DISABLE =
-
 define HOSTAPD_OPENWRT_PATCHES
 	cp -fpR $(BR2_EXTERNAL_TOMATO64_PATH)/package/hostapd-openwrt/src/. $(@D)
 endef
-
 HOSTAPD_OPENWRT_PRE_PATCH_HOOKS += HOSTAPD_OPENWRT_PATCHES
 
 define HOSTAPD_OPENWRT_COPY_CONFIG
 	cp -fpR $(BR2_EXTERNAL_TOMATO64_PATH)/package/hostapd-openwrt/files/hostapd-full.config $(@D)/hostapd/.config
 	cp -fpR $(BR2_EXTERNAL_TOMATO64_PATH)/package/hostapd-openwrt/files/wpa_supplicant-full.config $(@D)/wpa_supplicant/.config
-
-#	sed -i 's,CONFIG_UBUS=y,#CONFIG_UBUS=y,g' \
-#	$(@D)/hostapd/.config \
-#	$(@D)/wpa_supplicant/.config
-
 endef
-
 HOSTAPD_OPENWRT_POST_PATCH_HOOKS += HOSTAPD_OPENWRT_COPY_CONFIG
 
 HOSTAPD_OPENWRT_DEPENDENCIES += libopenssl
 HOSTAPD_OPENWRT_LIBS += `$(PKG_CONFIG_HOST_BINARY) --libs openssl`
-HOSTAPD_OPENWRT_CONFIG_EDITS += 's/\#\(CONFIG_TLS=openssl\)/\1/'
 
 HOSTAPD_OPENWRT_DEPENDENCIES += libnl-tiny
 HOSTAPD_OPENWRT_CFLAGS += -I$(STAGING_DIR)/usr/include/libnl-tiny
-HOSTAPD_OPENWRT_CONFIG_ENABLE += CONFIG_LIBNL32
 
 define HOSTAPD_OPENWRT_CONFIGURE_CMDS
 
 	echo ` \
-	CFLAGS="$(TARGET_CFLAGS) -I$(@D)/src -I$(@D)/src/utils -I$(@D)/src/crypto -DCONFIG_LIBNL20 -D_GNU_SOURCE -DCONFIG_MSG_MIN_PRIORITY=3 -Os -pipe -mcpu=cortex-a53 -fno-caller-saves -fno-plt -fhonour-copts -ffunction-sections -fdata-sections -flto=auto -fno-fat-lto-objects -Wformat -Werror=format-security -DPIC -fPIC -fstack-protector -D_FORTIFY_SOURCE=1 -Wl,-z,now -Wl,-z,relro" \
-	$(TARGET_CONFIGURE_OPTS) \
-	$(MAKE) \
+	CFLAGS="-I$(STAGING_DIR)/usr/include/libnl-tiny -I$(@D)/src/crypto -DCONFIG_LIBNL20 -D_GNU_SOURCE -DCONFIG_MSG_MIN_PRIORITY=3 -Os -pipe -mcpu=cortex-a53 -fno-caller-saves -fno-plt -ffunction-sections -fdata-sections -flto=auto -fno-fat-lto-objects -Wformat -Werror=format-security -DPIC -fPIC -fstack-protector -D_FORTIFY_SOURCE=1 -Wl,-z,now -Wl,-z,relro" \
+	$(MAKE) --no-print-directory \
 	-C $(@D)/hostapd \
+	AR="$(TARGET_CROSS)gcc-ar" \
+	AS="$(TARGET_CROSS)gcc -c -Os -pipe -mcpu=cortex-a53 -fno-caller-saves -fno-plt -ffunction-sections -fdata-sections -flto=auto -fno-fat-lto-objects -Wformat -Werror=format-security -DPIC -fPIC -fstack-protector -Wl,-z,now -Wl,-z,relro" \
+	LD="$(TARGET_CROSS)ld.bfd" \
+	NM="$(TARGET_CROSS)gcc-nm" \
+	CC="$(TARGET_CROSS)gcc" \
+	GCC="$(TARGET_CROSS)gcc" \
+	CXX="$(TARGET_CROSS)g++" \
+	RANLIB="$(TARGET_CROSS)gcc-ranlib" \
+	STRIP="$(TARGET_CROSS)strip" \
+	OBJCOPY="$(TARGET_CROSS)objcopy" \
+	OBJDUMP="$(TARGET_CROSS)objdump" \
+	SIZE="$(TARGET_CROSS)size" \
 	CONFIG_ACS=y \
 	CONFIG_DRIVER_NL80211=y \
 	CONFIG_IEEE80211AC=y \
@@ -70,15 +65,29 @@ define HOSTAPD_OPENWRT_CONFIGURE_CMDS
 	CONFIG_SUITEB192=y \
 	CONFIG_AP=y \
 	CONFIG_MESH=y \
-	LIBS="-fuse-ld=bfd -Wl,--gc-sections -flto=auto -fuse-linker-plugin -DPIC -fPIC -znow -zrelro -lcrypto -lssl -lubox -lubus -lblobmsg_json -lucode -lm -lnl-tiny -ludebug" \
+	LIBS="-L$(TARGET_DIR)/usr/lib -fuse-ld=bfd -Wl,--gc-sections -flto=auto -fuse-linker-plugin -DPIC -fPIC -znow -zrelro -lcrypto -lssl -lubox -lubus -lblobmsg_json -lucode -lm -lnl-tiny -ludebug" \
 	LIBS_c="" \
+	AR="$(TARGET_CROSS)gcc-ar" \
 	BCHECK= \
 	V=1 \
 	-s \
 	MULTICALL=1 \
 	dump_cflags; \
-	$(MAKE) \
+	CFLAGS="-I$(STAGING_DIR)/usr/include/libnl-tiny -I$(@D)/src/crypto -DCONFIG_LIBNL20 -D_GNU_SOURCE -DCONFIG_MSG_MIN_PRIORITY=3 -Os -pipe -mcpu=cortex-a53 -fno-caller-saves -fno-plt -ffunction-sections -fdata-sections -flto=auto -fno-fat-lto-objects -Wformat -Werror=format-security -DPIC -fPIC -fstack-protector -D_FORTIFY_SOURCE=1 -Wl,-z,now -Wl,-z,relro" \
+	$(MAKE) --no-print-directory \
 	-C $(@D)/wpa_supplicant \
+	AR="$(TARGET_CROSS)gcc-ar" \
+	AS="$(TARGET_CROSS)gcc -c -Os -pipe -mcpu=cortex-a53 -fno-caller-saves -fno-plt -ffunction-sections -fdata-sections -flto=auto -fno-fat-lto-objects -Wformat -Werror=format-security -DPIC -fPIC -fstack-protector -Wl,-z,now -Wl,-z,relro" \
+	LD="$(TARGET_CROSS)ld.bfd" \
+	NM="$(TARGET_CROSS)gcc-nm" \
+	CC="$(TARGET_CROSS)gcc" \
+	GCC="$(TARGET_CROSS)gcc" \
+	CXX="$(TARGET_CROSS)g++" \
+	RANLIB="$(TARGET_CROSS)gcc-ranlib" \
+	STRIP="$(TARGET_CROSS)strip" \
+	OBJCOPY="$(TARGET_CROSS)objcopy" \
+	OBJDUMP="$(TARGET_CROSS)objdump" \
+	SIZE="$(TARGET_CROSS)size" \
 	CONFIG_ACS=y \
 	CONFIG_DRIVER_NL80211=y \
 	CONFIG_IEEE80211AC=y \
@@ -92,23 +101,36 @@ define HOSTAPD_OPENWRT_CONFIGURE_CMDS
 	CONFIG_SUITEB192=y \
 	CONFIG_AP=y \
 	CONFIG_MESH=y \
-	LIBS="-fuse-ld=bfd -Wl,--gc-sections -flto=auto -fuse-linker-plugin -DPIC -fPIC -znow -zrelro -lcrypto -lssl -lubox -lubus -lblobmsg_json -lucode -lm -lnl-tiny -ludebug" \
+	LIBS="-L$(TARGET_DIR)/usr/lib -fuse-ld=bfd -Wl,--gc-sections -flto=auto -fuse-linker-plugin -DPIC -fPIC -znow -zrelro -lcrypto -lssl -lubox -lubus -lblobmsg_json -lucode -lm -lnl-tiny -ludebug" \
 	LIBS_c="" \
+	AR="$(TARGET_CROSS)gcc-ar" \
 	BCHECK= \
 	V=1 \
 	-s \
 	MULTICALL=1 \
-	dump_cflags \
-	| sed -e 's,-n ,,g' -e 's^-Os -pipe -mcpu=cortex-a53 -fno-caller-saves -fno-plt -fhonour-copts -ffunction-sections -fdata-sections -flto=auto -fno-fat-lto-objects -Wformat -Werror=format-security -DPIC -fPIC -fstack-protector -D_FORTIFY_SOURCE=1 -Wl,-z,now -Wl,-z,relro^^' \
-	` > $(@D)/.cflags
+	dump_cflags | sed -e 's,-n ,,g' -e 's^-Os -pipe -mcpu=cortex-a53 -fno-caller-saves -fno-plt -ffunction-sections -fdata-sections -flto=auto -fno-fat-lto-objects -Wformat -Werror=format-security -DPIC -fPIC -fstack-protector -D_FORTIFY_SOURCE=1 -Wl,-z,now -Wl,-z,relro^^' ` > $(@D)/.cflags
+
+sed -i 's/"/\\"/g' $(@D)/.cflags
 
 endef
 
 define HOSTAPD_OPENWRT_BUILD_CMDS
-	CFLAGS="$(TARGET_CFLAGS) $(cat $(@D)/.cflags) -I$(@D)/src -I$(@D)/src/utils -I$(@D)/src/crypto -DCONFIG_LIBNL20 -D_GNU_SOURCE -DCONFIG_MSG_MIN_PRIORITY=3 -Os -pipe -mcpu=cortex-a53 -fno-caller-saves -fno-plt -fhonour-copts -ffunction-sections -fdata-sections -flto=auto -fno-fat-lto-objects -Wformat -Werror=format-security -DPIC -fPIC -fstack-protector -D_FORTIFY_SOURCE=1 -Wl,-z,now -Wl,-z,relro" \
-	$(TARGET_CONFIGURE_OPTS) \
+
+	CFLAGS="-I$(STAGING_DIR)/usr/include/libnl-tiny -I$(@D)/src/crypto -DCONFIG_LIBNL20 -D_GNU_SOURCE -DCONFIG_MSG_MIN_PRIORITY=3 -Os -pipe -mcpu=cortex-a53 -fno-caller-saves -fno-plt -ffunction-sections -fdata-sections -flto=auto -fno-fat-lto-objects -Wformat -Werror=format-security -DPIC -fPIC -fstack-protector -Wl,-z,now -Wl,-z,relro" \
 	$(MAKE) \
 	-C $(@D)/hostapd \
+	AR="$(TARGET_CROSS)gcc-ar" \
+	AS="$(TARGET_CROSS)gcc -c -Os -pipe -mcpu=cortex-a53 -fno-caller-saves -fno-plt -ffunction-sections -fdata-sections -flto=auto -fno-fat-lto-objects -Wformat -Werror=format-security -DPIC -fPIC -fstack-protector -D_FORTIFY_SOURCE=1 -Wl,-z,now -Wl,-z,relro" \
+	LD="$(TARGET_CROSS)ld.bfd" \
+	NM="$(TARGET_CROSS)gcc-nm" \
+	CC="$(TARGET_CROSS)gcc" \
+	GCC="$(TARGET_CROSS)gcc" \
+	CXX="$(TARGET_CROSS)g++" \
+	RANLIB="$(TARGET_CROSS)gcc-ranlib" \
+	STRIP="$(TARGET_CROSS)strip" \
+	OBJCOPY="$(TARGET_CROSS)objcopy" \
+	OBJDUMP="$(TARGET_CROSS)objdump" \
+	SIZE="$(TARGET_CROSS)size" \
 	CONFIG_ACS=y \
 	CONFIG_DRIVER_NL80211=y \
 	CONFIG_IEEE80211AC=y \
@@ -122,18 +144,31 @@ define HOSTAPD_OPENWRT_BUILD_CMDS
 	CONFIG_SUITEB192=y \
 	CONFIG_AP=y \
 	CONFIG_MESH=y \
-	LIBS="-fuse-ld=bfd -Wl,--gc-sections -flto=auto -fuse-linker-plugin -DPIC -fPIC -znow -zrelro -lcrypto -lssl -lubox -lubus -lblobmsg_json -lucode -lm -lnl-tiny -ludebug" \
+	LIBS="-L$(TARGET_DIR)/usr/lib -fuse-ld=bfd -Wl,--gc-sections -flto=auto -fuse-linker-plugin -DPIC -fPIC -znow -zrelro -lcrypto -lssl -lubox -lubus -lblobmsg_json -lucode -lm -lnl-tiny -ludebug" \
 	LIBS_c="" \
+	AR="$(TARGET_CROSS)gcc-ar" \
 	BCHECK= \
 	V=1  \
+	CFLAGS='$(shell cat $(@D)/.cflags)' \
 	MULTICALL=1 \
 	hostapd_cli \
 	hostapd_multi.a
 
-	CFLAGS="$(TARGET_CFLAGS) $(cat $(@D)/.cflags) -I$(@D)/src -I$(@D)/src/utils -I$(@D)/src/crypto -DCONFIG_LIBNL20 -D_GNU_SOURCE -DCONFIG_MSG_MIN_PRIORITY=3 -Os -pipe -mcpu=cortex-a53 -fno-caller-saves -fno-plt -fhonour-copts -ffunction-sections -fdata-sections -flto=auto -fno-fat-lto-objects -Wformat -Werror=format-security -DPIC -fPIC -fstack-protector -D_FORTIFY_SOURCE=1 -Wl,-z,now -Wl,-z,relro" \
-	$(TARGET_CONFIGURE_OPTS) \
+	CFLAGS="-I$(STAGING_DIR)/usr/include/libnl-tiny -I$(@D)/src/crypto -DCONFIG_LIBNL20 -D_GNU_SOURCE -DCONFIG_MSG_MIN_PRIORITY=3 -Os -pipe -mcpu=cortex-a53 -fno-caller-saves -fno-plt -ffunction-sections -fdata-sections -flto=auto -fno-fat-lto-objects -Wformat -Werror=format-security -DPIC -fPIC -fstack-protector -Wl,-z,now -Wl,-z,relro" \
 	$(MAKE) \
 	-C $(@D)/wpa_supplicant \
+	AR="$(TARGET_CROSS)gcc-ar" \
+	AS="$(TARGET_CROSS)gcc -c -Os -pipe -mcpu=cortex-a53 -fno-caller-saves -fno-plt -ffunction-sections -fdata-sections -flto=auto -fno-fat-lto-objects -Wformat -Werror=format-security -DPIC -fPIC -fstack-protector -D_FORTIFY_SOURCE=1 -Wl,-z,now -Wl,-z,relro" \
+	LD="$(TARGET_CROSS)ld.bfd" \
+	NM="$(TARGET_CROSS)gcc-nm" \
+	CC="$(TARGET_CROSS)gcc" \
+	GCC="$(TARGET_CROSS)gcc" \
+	CXX="$(TARGET_CROSS)g++" \
+	RANLIB="$(TARGET_CROSS)gcc-ranlib" \
+	STRIP="$(TARGET_CROSS)strip" \
+	OBJCOPY="$(TARGET_CROSS)objcopy" \
+	OBJDUMP="$(TARGET_CROSS)objdump" \
+	SIZE="$(TARGET_CROSS)size" \
 	CONFIG_ACS=y \
 	CONFIG_DRIVER_NL80211=y \
 	CONFIG_IEEE80211AC=y \
@@ -147,17 +182,26 @@ define HOSTAPD_OPENWRT_BUILD_CMDS
 	CONFIG_SUITEB192=y \
 	CONFIG_AP=y \
 	CONFIG_MESH=y \
-	LIBS="-fuse-ld=bfd -Wl,--gc-sections -flto=auto -fuse-linker-plugin -DPIC -fPIC -znow -zrelro -lcrypto -lssl -lubox -lubus -lblobmsg_json -lucode -lm -lnl-tiny -ludebug" \
+	LIBS="-L$(TARGET_DIR)/usr/lib -fuse-ld=bfd -Wl,--gc-sections -flto=auto -fuse-linker-plugin -DPIC -fPIC -znow -zrelro -lcrypto -lssl -lubox -lubus -lblobmsg_json -lucode -lm -lnl-tiny -ludebug" \
 	LIBS_c="" \
+	AR="$(TARGET_CROSS)gcc-ar" \
 	BCHECK= \
 	V=1  \
+	CFLAGS='$(shell cat $(@D)/.cflags)' \
 	MULTICALL=1 \
 	wpa_cli \
 	wpa_supplicant_multi.a
 
 	$(TARGET_CC) \
 	-o $(@D)/wpad \
-	-Os -pipe -mcpu=cortex-a53 -fno-caller-saves -fno-plt -ffunction-sections -fdata-sections -flto=auto -fno-fat-lto-objects -Wformat -Werror=format-security -DPIC -fPIC -fstack-protector -D_FORTIFY_SOURCE=1 -Wl,-z,now -Wl,-z,relro $(BR2_EXTERNAL_TOMATO64_PATH)/package/hostapd-openwrt/files/multicall.c $(@D)/hostapd/hostapd_multi.a $(@D)/wpa_supplicant/wpa_supplicant_multi.a -fuse-ld=bfd -Wl,--gc-sections -flto=auto -fuse-linker-plugin -DPIC -fPIC -znow -zrelro -lcrypto -lssl -lubox -lubus -lblobmsg_json -lucode -lm -lnl-3 -lnl-genl-3 -ludebug
+	-Os -pipe -mcpu=cortex-a53 -fno-caller-saves -fno-plt -ffunction-sections -fdata-sections -flto=auto -fno-fat-lto-objects \
+	-Wformat -Werror=format-security -DPIC -fPIC -fstack-protector -D_FORTIFY_SOURCE=1 -Wl,-z,now -Wl,-z,relro \
+	$(BR2_EXTERNAL_TOMATO64_PATH)/package/hostapd-openwrt/files/multicall.c \
+	$(@D)/hostapd/hostapd_multi.a \
+	$(@D)/wpa_supplicant/wpa_supplicant_multi.a \
+	-L$(TARGET_DIR)/usr/lib \
+	-fuse-ld=bfd -Wl,--gc-sections -flto=auto -fuse-linker-plugin -DPIC -fPIC -znow -zrelro \
+	-lcrypto -lssl -lubox -lubus -lblobmsg_json -lucode -lm -lnl-tiny -ludebug
 
 endef
 
