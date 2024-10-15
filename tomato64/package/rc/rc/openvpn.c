6995d293bb96d74295a03091ea9cdc95066b3d73
lancethepants <lancethepants@gmail.com>
2024-10-15 14:21:31 -0600

    FT: rc: openvpn.c: simplify code
    Update from FreshTomato
    https://github.com/pedro0311/freshtomato-arm/commit/e88e18fe1da0e12463c6b738b34da982a351a496

--- tomato64/package/rc/rc/openvpn.c
+++ tomato64/package/rc/rc/openvpn.c
@@ -329,6 +329,8 @@ void start_ovpn_client(int unit)
 #if defined(TCONFIG_BCMARM) && defined(TCONFIG_BCMSMP)
 	char cpulist[2];
 	int cpu_num = sysconf(_SC_NPROCESSORS_CONF) - 1;
+	if (cpu_num < 0)
+		cpu_num = 0;
 #endif
 
 	memset(buffer, 0, BUF_SIZE);
@@ -733,10 +735,6 @@ void start_ovpn_client(int unit)
 
 #if defined(TCONFIG_BCMARM) && defined(TCONFIG_BCMSMP)
 	/* Spread clients on cpu 1,0 or 1,2,3,0 (in that order) */
-	cpu_num = sysconf(_SC_NPROCESSORS_CONF) - 1;
-	if (cpu_num < 0)
-		cpu_num = 0;
-
 	snprintf(cpulist, sizeof(cpulist), "%d", (unit & cpu_num));
 	taskset_ret = cpu_eval(NULL, cpulist, buffer, "--cd", buffer2, "--config", "config.ovpn");
 
@@ -816,6 +814,8 @@ void start_ovpn_server(int unit)
 #if defined(TCONFIG_BCMARM) && defined(TCONFIG_BCMSMP)
 	char cpulist[2];
 	int cpu_num = sysconf(_SC_NPROCESSORS_CONF) - 1;
+	if (cpu_num < 0)
+		cpu_num = 0;
 #endif
 
 	memset(buffer, 0, BUF_SIZE);
@@ -1349,10 +1349,6 @@ void start_ovpn_server(int unit)
 
 #if defined(TCONFIG_BCMARM) && defined(TCONFIG_BCMSMP)
 	/* Spread servers on cpu 1,0 or 1,2 (in that order) */
-	cpu_num = sysconf(_SC_NPROCESSORS_CONF) - 1;
-	if (cpu_num < 0)
-		cpu_num = 0;
-
 	snprintf(cpulist, sizeof(cpulist), "%d", (unit & cpu_num));
 	taskset_ret = cpu_eval(NULL, cpulist, buffer, "--cd", buffer2, "--config", "config.ovpn");
 
