3ddea13cf3c90a28c2f4e88f734f9e43e0111c16
lancethepants <lancethepants@gmail.com>
2024-10-15 10:26:36 -0600

    FT: httpd: iperf: use logmsg(); cosmetic
    Update from FreshTomato
    https://github.com/pedro0311/freshtomato-arm/commit/4f57d9e2566cd38b665f60649b6b8ba018bf6b13

--- tomato64/package/httpd/httpd/iperf.c
+++ tomato64/package/httpd/httpd/iperf.c
@@ -1,69 +1,84 @@
 /*
+ *
+ * FreshTomato Firmware
+ * Copyright (C) 2018 Michal Obrembski
+ *
+ * Fixes/updates (C) 2019 - 2024 pedro
+ *
+ */
 
-	Tomato Firmware
-	Copyright (C) 2018 Michal Obrembski
-
-*/
 
 #include "tomato.h"
-#include "httpd.h"
 #include <sys/stat.h>
 
-#include <shared.h>
+#define iperf_log	"/tmp/iperf_log"
+#define iperf_interval	"/tmp/iperf_interval"
+#define iperf_pidfile	"/var/run/iperf.pid"
+
+/* needed by logmsg() */
+#define LOGMSG_DISABLE	DISABLE_SYSLOG_OSM
+#define LOGMSG_NVDEBUG	"iperf_debug"
+
 
 /*
-Always clear log files before run!
-IF IPERF is running and iperf.pid is present and INTERVAL is not present -> SERVER awaiting OK
-IF IPERF is running and iperf.pid is present and INTERVAL is present -> SERVER receivin OK
-IF IPERF is not running but unclar(INTERVAL) and logfile present -> Server finished
-IF IPERF is running and no iperf.pid file is present and INTERVAL is present -> Client receiving
-IF IPERF is running and no iperf.pid file is present and log is present -> Client receiving
-*/
+ * Always clear log files before run!
+ * IF IPERF is running and iperf.pid is present and INTERVAL is not present -> SERVER awaiting OK
+ * IF IPERF is running and iperf.pid is present and INTERVAL is present -> SERVER receiving OK
+ * IF IPERF is not running but unclar(INTERVAL) and logfile present -> Server finished
+ * IF IPERF is running and no iperf.pid file is present and INTERVAL is present -> Client receiving
+ * IF IPERF is running and no iperf.pid file is present and log is present -> Client receiving
+ */
+
 
 void wo_ttcpstatus(char *url)
 {
-	int iperf_pid;
+	pid_t iperf_pid;
 	struct stat st;
-	if (access( "/tmp/iperf_log", F_OK ) != -1) {
+
+	if (access(iperf_log, F_OK ) != -1) {
 		/* OK, we got log but it may be empty (iperf just started) */
-		stat("/tmp/iperf_log", &st);
-		printf("Size of iperflog: %jd\n", (intmax_t)st.st_size);
+		stat(iperf_log, &st);
+		logmsg(LOG_DEBUG, "*** %s: %d: Size of iperflog: %jd", __FUNCTION__, __LINE__, (intmax_t)st.st_size);
+
 		if (st.st_size > 0 ) {
-			printf("Sending content of iperf log\n");
-			do_file("/tmp/iperf_log");
+			logmsg(LOG_DEBUG, "*** %s: %d: Sending content of iperf log", __FUNCTION__, __LINE__);
+			do_file(iperf_log);
 			return;
 		}
 	}
+
 	iperf_pid = pidof("iperf");
-	_dprintf("PID of iperf: %d\n", iperf_pid);
+	logmsg(LOG_DEBUG, "*** %s: %d: PID of iperf: %d", __FUNCTION__, __LINE__, iperf_pid);
+
 	if (iperf_pid < 0) {
 		web_puts("{ \"mode\": \"Stopped\" }");
 		return;
 	}
-	if (access( "/var/run/iperf.pid", F_OK ) != -1) {
+	if (access(iperf_pidfile, F_OK ) != -1) {
 		/* We know we're in server mode */
-		_dprintf("iperf.pid found, server mode\n");
-		if (access( "/tmp/iperf_interval", F_OK ) != -1) {
-			_dprintf("Sending contents of interval\n");
+		logmsg(LOG_DEBUG, "*** %s: %d: iperf.pid found, server mode", __FUNCTION__, __LINE__);
 
-			stat("/tmp/iperf_interval", &st);
-			if (st.st_size > 0) {
-				do_file("/tmp/iperf_interval");
-			} else {
+		if (access(iperf_interval, F_OK ) != -1) {
+			logmsg(LOG_DEBUG, "*** %s: %d: Sending contents of interval", __FUNCTION__, __LINE__);
+
+			stat(iperf_interval, &st);
+			if (st.st_size > 0)
+				do_file(iperf_interval);
+			else
 				web_puts("{ \"mode\": \"Server waiting\"}");
-			}
-		} else {
+		}
+		else
 			web_puts("{ \"mode\": \"Server waiting\"}");
-		}
-	} else {
-		_dprintf("Client mode, sending interval\n");
+	}
+	else {
 		/* We know we're in client mode */
-		stat("/tmp/iperf_interval", &st);
-		if (st.st_size > 0) {
-			do_file("/tmp/iperf_interval");
-		} else {
+		logmsg(LOG_DEBUG, "*** %s: %d: Client mode, sending interval", __FUNCTION__, __LINE__);
+
+		stat(iperf_interval, &st);
+		if (st.st_size > 0)
+			do_file(iperf_interval);
+		else
 			web_puts("{ \"mode\": \"Client preparing\"}");
-		}
 	}
 }
 
@@ -83,41 +98,44 @@ void wo_ttcprun(char *url)
 	int byteLimitMode = 0;		/* Time limit by default */
 	unsigned long long limit = 10;	/* 10 Seconds, by default */
 
-	unlink("/tmp/iperf_interval");
-	unlink("/tmp/iperf_log");
+	unlink(iperf_interval);
+	unlink(iperf_log);
 
 	port = atoi(webcgi_safeget("_port", "5201"));
 	udpMode = atoi(webcgi_safeget("_udpProto", "0"));
 	byteLimitMode = atoi(webcgi_safeget("_limitMode", "0"));
 	limit = strtoull(webcgi_safeget("_limit", "10"), NULL, 0);
-	_dprintf("port: %d UDP: %s LimitMode %s Limit %llu Mode\n", port, udpMode == 0 ? "NO" : "YES", byteLimitMode == 0 ? "TIME" : "BYTE", limit);
+	logmsg(LOG_DEBUG, "*** %s: %d: port: %d UDP: %s LimitMode: %s Limit %llu Mode", __FUNCTION__, __LINE__, port, udpMode == 0 ? "NO" : "YES", byteLimitMode == 0 ? "TIME" : "BYTE", limit);
 
 	if ((v = webcgi_get("_mode")) != NULL && (*v)) {
 		snprintf(tmp, sizeof(tmp), "%d", port);
-		if (!strcmp(v, "server")) {
-			_dprintf("Server\n");
-			snprintf(cmdBuffer, sizeof(cmdBuffer), "iperf -J --logfile /tmp/iperf_log --intervalfile \
-			        /tmp/iperf_interval -I /var/run/iperf.pid -s -1 -D -p %d", port);
+		if (!strcmp(v, "server")) { /* server */
+			logmsg(LOG_INFO, "iperf started in server mode");
+			snprintf(cmdBuffer, sizeof(cmdBuffer), "iperf -J --logfile "iperf_log" --intervalfile "iperf_interval" -I "iperf_pidfile" -s -1 -D -p %d", port);
 		}
-		else {
+		else { /* client */
 			if ((host = webcgi_get("_host")) != NULL && (*host)) {
 				if ((strstr(host, "/") > 0) || (strstr(host, ";") > 0) || (strstr(host, "`") > 0))
 					return;
 
-				_dprintf("Client Address %s\n", host);
-				snprintf(cmdBuffer, sizeof(cmdBuffer), "iperf -J --logfile /tmp/iperf_log --intervalfile \
-				        /tmp/iperf_interval -p %d %s %s %llu -c %s &", port, udpMode == 1 ? "-u" : "", byteLimitMode == 1 ? "-n" : "-t", limit, host);
+				logmsg(LOG_INFO, "iperf started in client mode, address %s", host);
+				snprintf(cmdBuffer, sizeof(cmdBuffer), "iperf -J --logfile "iperf_log" --intervalfile "iperf_interval" -p %d %s %s %llu -c %s &", port, udpMode == 1 ? "-u" : "", byteLimitMode == 1 ? "-n" : "-t", limit, host);
 			}
 		}
-		_dprintf("Running command:\n %s \n", cmdBuffer);
+		logmsg(LOG_DEBUG, "*** %s: %d: Running command: %s", __FUNCTION__, __LINE__, cmdBuffer);
 		run_program(cmdBuffer);
 	}
-	_dprintf("\n");
 }
 
 void wo_ttcpkill(char *url)
 {
+	pid_t pid;
+	pid = pidof("iperf");
+
 	killall("iperf", SIGTERM);
-	unlink("/tmp/iperf_interval");
-	unlink("/tmp/iperf_log");
+	unlink(iperf_interval);
+	unlink(iperf_log);
+
+	if (pid > 0)
+		logmsg(LOG_INFO, "iperf stopped");
 }
