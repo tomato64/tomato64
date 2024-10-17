53034e90cfecefdcebb12c10c71b68e6fcfca2ab
lancethepants <lancethepants@gmail.com>
2024-10-17 15:27:54 -0600

    netifd: install scripts to target

--- tomato64/package/netifd/netifd.mk
+++ tomato64/package/netifd/netifd.mk
@@ -18,4 +18,13 @@ NETIFD_CONF_OPTS += \
 	-DCMAKE_C_FLAGS="$(TARGET_CFLAGS) $(NETIFD_CFLAGS)" \
 	-DCMAKE_EXE_LINKER_FLAGS="-lnl-tiny" \
 
+define NETIFD_POST_INSTALL_SCRIPTS
+	$(INSTALL) -d $(TARGET_DIR)/lib/netifd
+	$(INSTALL) -D -m 0644 $(@D)/scripts/netifd-proto.sh $(TARGET_DIR)/lib/netifd
+	$(INSTALL) -D -m 0644 $(@D)/scripts/netifd-wireless.sh $(TARGET_DIR)/lib/netifd
+	$(INSTALL) -D -m 0644 $(@D)/scripts/utils.sh $(TARGET_DIR)/lib/netifd
+endef
+
+NETIFD_POST_INSTALL_TARGET_HOOKS += NETIFD_POST_INSTALL_SCRIPTS
+
 $(eval $(cmake-package))
