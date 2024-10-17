3274a827254848612761ad9bad7b6781472cea33
lancethepants <lancethepants@gmail.com>
2024-10-17 15:06:04 -0600

    GL-MT6000: add uci symlink.

--- tomato64/board/x86_64/post-build-fs.sh
+++ tomato64/board/x86_64/post-build-fs.sh
@@ -78,5 +78,11 @@ ln -sf /www/ext/proxy.pac $TARGET_DIR/www/wpad.dat
 USB_MODESWITCH_DIR=$BUILD_DIR/$(ls $BUILD_DIR --ignore='*data*' | grep usb_modeswitch)
 sed -e '/^\s*#.*$/d' -e '/^\s*$/d' < $USB_MODESWITCH_DIR/usb_modeswitch.conf > $TARGET_DIR/rom/etc/usb_modeswitch.conf
 
+#GL-MT6000 specific edits
+if grep -q ^BR2_aarch64=y ${BR2_CONFIG}
+then
+	ln -sf /usr/bin/uci $TARGET_DIR/sbin/uci
+fi
+
 # Remove unneeded mariadb stuff
 cd $TARGET_DIR/usr/bin && rm -f myisam_ftdump myisamlog myisampack mysql_client_test mariadb-client-test mysql_client_test_embedded mariadb-client-test-embedded mysql_convert_table_format mariadb-convert-table-format mysql_embedded mariadb-embedded mysql_find_rows mariadb-find-rows mysql_fix_extensions mariadb-fix-extensions mysql_plugin mariadb-plugin mysql_secure_installation mariadb-secure-installation mysql_setpermission mariadb-setpermission mysql_tzinfo_to_sql mariadb-tzinfo-to-sql mysql_upgrade mariadb-upgrade mysql_waitpid mariadb-waitpid mysqlaccess mariadb-access mysqlbinlog mariadb-binlog mysqlcheck mariadb-check mysqld_multi mariadbd-multi mysqld_safe mariadbd-safe mysqld_safe_helper mariadbd-safe-helper mysqldumpslow mariadb-dumpslow mysqlhotcopy mariadb-hotcopy mysqlimport mariadb-import mysqlshow mariadb-show mysqlslap mariadb-slap mysqltest mariadb-test mysqltest_embedded mariadb-test-embedded msql2mysql wsrep_sst_backup wsrep_sst_common wsrep_sst_mariabackup wsrep_sst_mysqldump wsrep_sst_rsync wsrep_sst_rsync_wan aria_chk aria_dump_log aria_ftdump aria_pack aria_read_log aria_s3_copy mariabackup mariadb-backup mariadb-config
