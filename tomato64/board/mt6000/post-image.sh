148bbb771847b23375c5f0278544b918e8634ed8
lancethepants <lancethepants@gmail.com>
2024-12-17 12:59:57 -0700

    MT6000: Simplify the sysupgrade bin name.

--- tomato64/board/mt6000/post-image.sh
+++ tomato64/board/mt6000/post-image.sh
@@ -26,10 +26,10 @@ $BR2_EXTERNAL_TOMATO64_PATH/board/mt6000/sysupgrade-tar.sh \
 --board glinet_gl-mt6000 \
 --kernel $BINARIES_DIR/glinet_gl-mt6000-kernel.bin \
 --rootfs $BINARIES_DIR/rootfs.ext4 \
-$BINARIES_DIR/tomato64-mediatek-filogic-glinet_gl-mt6000-ext4-sysupgrade.bin
+$BINARIES_DIR/tomato64-gl-mt6000-sysupgrade.bin
 
 VERSION="$(cat $BR2_EXTERNAL_TOMATO64_PATH/version)"
 COMMIT="$(git log -n 1 --pretty=format:"%H")"
 
 echo "{  \"metadata_version\": \"1.1\", \"compat_version\": \"1.0\",   \"supported_devices\":[\"glinet,gl-mt6000\"], \"version\": { \"release\": \"$(echo $VERSION)\", \"date\": \"$(date '+%Y%m%d%H%M%S')\", \"dist\": \"Tomato64\", \"version\": \"$(echo $VERSION)\", \"revision\": \"$(echo $COMMIT)\", \"target\": \"mediatek/filogic\", \"board\": \"glinet_gl-mt6000\" } }" \
-| fwtool -I - $BINARIES_DIR/tomato64-mediatek-filogic-glinet_gl-mt6000-ext4-sysupgrade.bin
+| fwtool -I - $BINARIES_DIR/tomato64-gl-mt6000-sysupgrade.bin
