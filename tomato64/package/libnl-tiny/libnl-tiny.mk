3532fb4609b15ae0b892507b6b2fbc56865e4863
lancethepants <lancethepants@gmail.com>
2025-04-14 14:14:14 -0600

    libnl-tiny: sync to OpenWrt 24.10.1

--- tomato64/package/libnl-tiny/libnl-tiny.mk
+++ tomato64/package/libnl-tiny/libnl-tiny.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBNL_TINY_VERSION = 965c4bf49658342ced0bd6e7cb069571b4a1ddff
+LIBNL_TINY_VERSION = c0df580adbd4d555ecc1962dbe88e91d75b67a4e
 LIBNL_TINY_SITE = https://git.openwrt.org/project/libnl-tiny.git
 LIBNL_TINY_SITE_METHOD=git
 LIBNL_TINY_INSTALL_STAGING = YES
