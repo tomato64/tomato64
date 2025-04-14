cf822a56675f4aeb7132ceb75bcc204376ff770a
lancethepants <lancethepants@gmail.com>
2025-04-14 14:22:47 -0600

    mt76: sync with OpenWrt v24.10.1

--- tomato64/package/mt76/mt76.mk
+++ tomato64/package/mt76/mt76.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MT76_VERSION = 8e4f72b682e9070108536507c5e2720b18c3816d
+MT76_VERSION = e5fef138524e63314cb96ff8314048d175294e95
 MT76_SITE = $(call github,openwrt,mt76,$(MT76_VERSION))
 MT76_LICENSE = GPL-2.0
 MT76_DEPENDENCIES = linux mac80211
