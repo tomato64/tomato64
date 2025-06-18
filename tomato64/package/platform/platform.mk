32450a3746edc579246b91e8b189058e89cab8c6
lancethepants <lancethepants@gmail.com>
2025-06-18 13:30:00 -0600

    Add BR2_PACKAGE_PLATFORM_ARM64 target
    Fixes hotplug2 not patching

--- tomato64/package/platform/platform.mk
+++ tomato64/package/platform/platform.mk
@@ -8,16 +8,17 @@ ifeq ($(BR2_PACKAGE_PLATFORM_X86_64),y)
 export PLATFORM_X86_64=y
 endif
 
-ifeq ($(BR2_PACKAGE_PLATFORM_MT6000),y)
-export PLATFORM_MT6000=y
+ifeq ($(BR2_PACKAGE_PLATFORM_ARM64),y)
 export PLATFORM_ARM64=y
 export PLATFORM_WIFI=y
 endif
 
+ifeq ($(BR2_PACKAGE_PLATFORM_MT6000),y)
+export PLATFORM_MT6000=y
+endif
+
 ifeq ($(BR2_PACKAGE_PLATFORM_BPIR3MINI),y)
 export PLATFORM_BPIR3MINI=y
-export PLATFORM_ARM64=y
-export PLATFORM_WIFI=y
 endif
 
 $(eval $(generic-package))
