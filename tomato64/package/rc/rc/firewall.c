29104c247c22fe792395f66eeac5c1590386a848
lancethepants <lancethepants@gmail.com>
2025-10-30 07:14:16 -0600

    FT: rc: firewall.c: reduce hitcount for www to 20 - MIPS doesn't allow higher values
    Update from FreshTomato
    https://github.com/FreshTomato-Project/freshtomato-arm/commit/cca4b435a62b10a6a99d21533523afaa6e6c9fe0

--- tomato64/package/rc/rc/firewall.c
+++ tomato64/package/rc/rc/firewall.c
@@ -1119,7 +1119,7 @@ static void filter_input(void)
 
 		ipt_write("-N wwwlimit\n"
 		          "-A wwwlimit -m recent --set --name www\n"
-		          "-A wwwlimit -m recent --update --hitcount 30 --seconds 5 --name www -j %s\n",
+		          "-A wwwlimit -m recent --update --hitcount 20 --seconds 3 --name www -j %s\n",
 		          chain_in_drop);
 
 		if (nvram_get_int("dmz_enable") && nvram_get_int("dmz_ra"))
@@ -1661,7 +1661,7 @@ static void filter6_input(void)
 
 		ip6t_write("-N wwwlimit\n"
 		           "-A wwwlimit -m recent --set --name www\n"
-		           "-A wwwlimit -m recent --update --hitcount 30 --seconds 5 --name www -j %s\n",
+		           "-A wwwlimit -m recent --update --hitcount 20 --seconds 3 --name www -j %s\n",
 		           chain_in_drop);
 
 		if (nvram_get_int("dmz_enable") && nvram_get_int("dmz_ra"))
