7ebb40bac2bc48ea069a0d82e1a334514073fd96
lancethepants <lancethepants@gmail.com>
2025-11-03 14:40:46 -0700

    FT: others: simple_lock_unlock.sh: extend timeout, change logging level
    Update from FreshTomato
    https://github.com/FreshTomato-Project/freshtomato-arm/commit/af149f488be4f028ca6f297ef5e6732382add7fd

--- tomato64/package/others/others/simple_lock_unlock.sh
+++ tomato64/package/others/others/simple_lock_unlock.sh
@@ -3,16 +3,17 @@
 
 PID=$$
 FN="/var/lock/firewall.lock"
-LOGS="logger -t simple_lock_unlock.sh[$PID]"
+LOGW="logger -p WARN -t simple_lock_unlock.sh[$PID]"
+TIMEOUT=8
 
 
 simple_lock() {
-	local n=$((5 + ($PID % 10)))
+	local n=$(($TIMEOUT + ($PID % 10)))
 
 	while ! rm -- "$FN" 2>/dev/null; do
 		n=$((n - 1))
 		if [ "$n" -eq 0 ]; then
-			$LOGS "breaking $FN"
+			$LOGW "breaking $FN"
 			break
 		fi
 		sleep 1
